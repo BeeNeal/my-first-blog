5c44531a9c16287e031c507c92c4e380e211936f
BeeNeal <brittanyneal22@gmail.com>
2019-11-13 11:21:55 -0800

    adding template language to show posts from DB

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+# The dot before models means current directory or current application. Both 
+# views.py and models.py are in the same directory. This means we can use . and 
+# the name of the file (without .py). Then we import the name of the model.
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
