5018fffbce0c09444a7c652acdb816ab8fd3883e
BeeNeal <brittanyneal22@gmail.com>
2019-11-14 19:08:00 -0800

    added individual post page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # The dot before models means current directory or current application. Both 
 # views.py and models.py are in the same directory. This means we can use . and 
 # the name of the file (without .py). Then we import the name of the model.
@@ -9,3 +9,8 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    # good practice to design a nice "page not found" error page
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
