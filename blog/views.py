b9020b2b07a491e3b5dc3988fa25baa3926192c4
BeeNeal <brittanyneal22@gmail.com>
2019-11-15 17:51:32 -0800

    working save new post

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 # The dot before models means current directory or current application. Both 
 # views.py and models.py are in the same directory. This means we can use . and 
 # the name of the file (without .py). Then we import the name of the model.
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -14,3 +15,17 @@ def post_detail(request, pk):
     # good practice to design a nice "page not found" error page
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # not commiting until we add the author, usually will not use commit.false
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
